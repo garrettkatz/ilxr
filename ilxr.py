9b7f31adfb47f27fac6f7f5c0e9965bf3b78ad4d
Garrett Katz <garrettkatz@users.noreply.github.com>
2026-05-01 15:55:19 -0400

    prototyped ilxr method on toy

--- ilxr.py
+++ ilxr.py
@@ -1,1 +1,107 @@
-# todo
+import torch as tr
+
+def ilxr_gd(env, policy, batch_size, num_updates, learning_rate):
+    """
+    Updates policy in place
+    """
+
+    reward_curve = []
+    for update in range(num_updates):
+        # collect rollouts
+        with tr.no_grad():
+            states = env.rollout(num_steps, batch_size, policy)
+        rewards = env.reward(states)
+        D = states.shape[-1] # state dimension
+
+        net_reward = rewards.mean(dim=-1).sum()
+        print(f"{update}: net reward = {net_reward:.5f}")
+        reward_curve.append(net_reward)
+
+        # fit linear models
+        A, B, c, w, b = {}, {}, {}, {}, {}
+        for t in range(num_steps-1):
+
+            # dynamics
+            with tr.no_grad():
+                result = tr.linalg.lstsq(
+                    tr.cat([states[t], policy(t, states[t]), tr.ones(batch_size,1)], dim=-1),
+                    states[t+1])
+            dyn = result.solution
+            A[t], B[t], c[t] = dyn[:D], dyn[D:2*D], dyn[2*D:]
+
+            # reward
+            with tr.no_grad():
+                result = tr.linalg.lstsq(
+                    tr.cat([states[t+1], tr.ones(batch_size,1)], dim=-1),
+                    rewards[t+1])
+            rew = result.solution
+            w[t+1], b[t+1] = rew[:D], rew[D:]
+
+        # update policy
+        objective = tr.tensor(0.)
+        state = states[0]
+        for t in range(num_steps-1):
+            action = policy(t, state)
+            state = state @ A[t] + action @ B[t] + c[t]
+            reward = state @ w[t+1] + b[t+1]
+            objective += reward.mean()
+
+        objective.backward()
+        for t in range(num_steps-1):
+            policy.lins[t].weight.data += policy.lins[t].weight.grad * learning_rate
+            policy.lins[t].bias.data += policy.lins[t].bias.grad * learning_rate
+            policy.lins[t].weight.grad[:] = 0
+            policy.lins[t].bias.grad[:] = 0
+
+    return reward_curve
+
+if __name__ == "__main__":
+
+    import matplotlib.pyplot as pt
+    from toy import ToyEnv, TimeVaryingLinearPolicy
+
+    init_stdev = 0.05
+    num_steps = 30
+    delta_t = 2**.5 / num_steps
+    batch_size = 64
+    num_updates = 100
+    learning_rate = .1
+
+    env = ToyEnv(init_stdev, delta_t)
+    policy = TimeVaryingLinearPolicy(num_steps)
+
+    pt.figure(figsize=(12,4))
+
+    pt.subplot(1,3,1)
+    with tr.no_grad():
+        states = env.rollout(num_steps, 3, policy)
+    env.contours(num_pts=20, levels=20)
+    for b in range(states.shape[1]):
+        pt.plot(states[:,b,0], states[:,b,1], 'r.-')
+    pt.title("Before training")
+
+    reward_curve = ilxr_gd(env, policy, batch_size, num_updates, learning_rate)
+
+    print(tr.stack([lin.bias.data for lin in policy.lins]))
+
+    with tr.no_grad():
+        states = env.rollout(num_steps, 3, policy)
+
+    pt.subplot(1,3,2)
+    with tr.no_grad():
+        states = env.rollout(num_steps, 3, policy)
+    env.contours(num_pts=20, levels=20)
+    for b in range(states.shape[1]):
+        pt.plot(states[:,b,0], states[:,b,1], 'r.-')
+    pt.title("After training")
+
+    pt.subplot(1,3,3)
+    pt.plot(reward_curve)
+    pt.xlabel("Gradient update")
+    pt.ylabel("Net reward (batch average)")
+    pt.title("Reward Curve")
+
+    pt.tight_layout()
+    pt.savefig("toy.png")
+    pt.show()
+
--- toy.py
+++ toy.py
@@ -1,68 +1,72 @@
 """
 Simple toy environment
 """
-import numpy as np
 import torch as tr
+import matplotlib.pyplot as pt
 
-S0 = .01 # stdev in initial states
-DT = .1 # time delta for forward euler
+class ToyEnv:
+    def __init__(self, init_stdev, delta_t):
+        self.init_stdev = init_stdev # stdev in initial states
+        self.delta_t = delta_t # time delta for forward euler
 
-def step(state, action):
-    # return np.clip(state + action * DT, 0, 1)
-    return tr.clamp(state + action * DT, 0, 1)
+    def step(self, state, action):
+        # action = tr.clamp(action, -2, 2)
+        return tr.clamp(state + action * self.delta_t, 0, 1)
+    
+    def reward(self, states):
+        return \
+            tr.exp(-((states - tr.tensor([1.,1.]))**2).sum(dim=-1)/.2) - \
+            tr.exp(-((states - tr.tensor([.4,.2]))**2).sum(dim=-1)/.1) - \
+            tr.exp(-((states - tr.tensor([.6,.8]))**2).sum(dim=-1)/.1)    # return \
+    
+    def rollout(self, num_steps, batch_size, policy):
+        states = [(self.init_stdev*tr.randn(batch_size, 2)).abs()]
+        for t in range(num_steps-1):
+            states.append(self.step(states[t], policy(t, states[t])) )
+        return tr.stack(states)
 
-def reward(states):
-    return \
-        tr.exp(-((states - tr.tensor([1.,1.]))**2).sum(dim=-1)/.2) - \
-        tr.exp(-((states - tr.tensor([.4,.2]))**2).sum(dim=-1)/.1) - \
-        tr.exp(-((states - tr.tensor([.6,.8]))**2).sum(dim=-1)/.1)    # return \
-    #     np.exp(-((states - np.array([1.,1.]))**2).sum(axis=-1)/.2) - \
-    #     np.exp(-((states - np.array([.4,.2]))**2).sum(axis=-1)/.1) - \
-    #     np.exp(-((states - np.array([.6,.8]))**2).sum(axis=-1)/.1)
+    def contours(self, num_pts, levels):
+        X, Y = tr.meshgrid(tr.linspace(0,1,num_pts),tr.linspace(0,1,num_pts))
+        states = tr.stack([X.flatten(), Y.flatten()], axis=-1)
+        R = self.reward(states).reshape(X.shape)
+        pt.contourf(X, Y, R, levels=levels)
+        pt.colorbar()
 
-def rollout(num_steps, batch_size, policy):
-    # states = np.empty((num_steps, batch_size, 2))
-    # states[0] = np.fabs(S0*np.random.randn(batch_size, 2))
-    # for t in range(num_steps-1):
-    #     states[t+1] = step(states[t], policy(t, states[t]))
-    states = [(S0*tr.randn(batch_size, 2)).abs()]
-    for t in range(num_steps-1):
-        states.append( step(states[t], policy(t, states[t])) )
-    return tr.stack(states)
-
-
-# class LinearPolicy:
-#     def __init__(self, num_steps):
-#         self.weights = [np.zeros((2,2)) for _ in range(num_steps)]
-#         self.bias = [np.ones(2) for _ in range(num_steps)]
-#     def __call__(self, t, states):
-#         return states @ self.weights[t] + self.bias[t]
 class TimeVaryingLinearPolicy:
+
     def __init__(self, num_steps):
-        self.lins = [tr.nn.Linear(2,2) for _ in range(num_steps)]
+        self.lins = [tr.nn.Linear(2,2) for _ in range(num_steps-1)]
+        # init for straight diagonal trajectory
+        for lin in self.lins:
+            lin.weight.data[:] = 0.
+            lin.bias.data[:] = 1.
+
     def __call__(self, t, states):
         return self.lins[t](states)
 
 if __name__ == "__main__":
 
+    init_stdev = 0.05
     num_steps = 30
+    delta_t = 2**.5 / num_steps
 
-    X, Y = np.meshgrid(np.linspace(0,1,20),np.linspace(0,1,20))
-    states = np.stack([X.flatten(), Y.flatten()], axis=-1)
-    R = reward(states).reshape(X.shape)
+    env = ToyEnv(init_stdev, delta_t)
+
+    # X, Y = tr.meshgrid(tr.linspace(0,1,20),tr.linspace(0,1,20))
+    # states = tr.stack([X.flatten(), Y.flatten()], axis=-1)
+    # R = env.reward(states).reshape(X.shape)
 
     # do some rollouts
-    def random_policy(t, states):
-        return tr.randn(*states.shape)
-        # return np.random.randn(*states.shape)
-    linear_policy = LinearPolicy(num_steps)
+    def random_policy(t, states): return tr.randn(*states.shape)
+    linear_policy = TimeVaryingLinearPolicy(num_steps)
 
-    rstates = rollout(num_steps, 1, random_policy)
-    lstates = rollout(num_steps, 2, linear_policy)
+    with tr.no_grad():
+        rstates = env.rollout(num_steps, 1, random_policy)
+        lstates = env.rollout(num_steps, 2, linear_policy)
 
-    import matplotlib.pyplot as pt
-    pt.contourf(X, Y, R, levels=20)
-    pt.colorbar()
+    env.contours(num_pts=20, levels=20)
+    # pt.contourf(X, Y, R, levels=20)
+    # pt.colorbar()
     for states, color in [(rstates, 'b'), (lstates, 'r')]:
         for b in range(states.shape[1]):
             pt.plot(states[:,b,0], states[:,b,1], color+'.-')
